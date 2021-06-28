7f24f2fb48d745bfadf9acc08024c490b72ac34a
lukostelloe <lukostelloe@gmail.com>
2021-06-28 17:29:45 +0200

    some functions done

--- meteo.js
+++ meteo.js
@@ -4,17 +4,35 @@
 //   .then((response) => response.text())
 //   .then((data) => console.log(data));
 
-let cityname;
+const submitbtn = document.getElementById("submit");
+const inputbtn = document.getElementById("input");
+let weatherdiv = document.getElementById("container");
+let city = "";
 
-function getWeather(cityname) {
-  let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=124e98bd1e47dc311d703689c9585ae1`;
-  let response = fetch(url)
-    .then((response) => response.text())
-    .then((data) =>
-      console.log(
-        `Here's the weather information for ${cityname} .......................................................................................... ${data}`
-      )
-    );
-}
+submitbtn.addEventListener(
+  "click",
+  function (event) {
+    event.preventDefault();
+    city = inputbtn.value;
+    console.log(city);
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=124e98bd1e47dc311d703689c9585ae1`
+    )
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        weatherdiv.innerHTML = `Weather is ${data.weather[0].main}`;
+        return data;
+      });
+  },
+  false
+);
 
-getWeather("Dublin");
+// function getWeather(cityname) {
+//   let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=124e98bd1e47dc311d703689c9585ae1`;
+//   let response = fetch(url)
+//     .then((response) => response.text())
+//     .then((data) => (weatherdiv.innerHTML = `${cityname}....${data}`));
+// }
