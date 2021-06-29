825b444e16e9b9b6f68b4c0bd1aaa049800c9402
lukostelloe <lukostelloe@gmail.com>
2021-06-29 17:04:49 +0200

    adding graph

--- meteo.js
+++ meteo.js
@@ -1,9 +1,4 @@
-// let response = fetch(
-//   "http://api.openweathermap.org/data/2.5/weather?q=London&appid=124e98bd1e47dc311d703689c9585ae1"
-// )
-//   .then((response) => response.text())
-//   .then((data) => console.log(data));
-
+//DEFINE HTML ELEMENTS
 const submitbtn = document.getElementById("submit");
 const inputbtn = document.getElementById("input");
 const body = document.getElementById("body");
@@ -13,6 +8,9 @@ let containerdiv = document.getElementById("container");
 let citydiv = document.getElementById("citydiv");
 let weatherdiv = document.getElementById("weatherdiv");
 let tempdiv = document.getElementById("tempdiv");
+
+//DEFINING VARIABLES AND IMAGES
+
 let city = "";
 var cloudsimage = document.createElement("img");
 cloudsimage.src = "images/clouds.jpg";
@@ -21,10 +19,21 @@ rainimage.src = "images/rain.jpg";
 var clearimage = document.createElement("img");
 clearimage.src = "images/clear.jpg";
 
+//FETCH THE WEATHER API AND DISPLAY INFO ON HTML
+
 submitbtn.addEventListener("click", function (event) {
   event.preventDefault();
   city = inputbtn.value;
   console.log(city);
+  //FETCH WEATHER OVER 5 DAYS
+  fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=124e98bd1e47dc311d703689c9585ae1`
+  )
+    .then((result2) => result2.json())
+    .then((data2) => {
+      console.log(data2);
+    });
+  //FETCH WEATHER FOR NOW
   fetch(
     `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=124e98bd1e47dc311d703689c9585ae1`
   )
@@ -55,3 +64,24 @@ submitbtn.addEventListener("click", function (event) {
       }
     });
 });
+
+const config = {
+  type: "line",
+  data,
+  options: {},
+};
+
+const labels = ["January", "February", "March", "April", "May", "June"];
+const data = {
+  labels: labels,
+  datasets: [
+    {
+      label: "My First dataset",
+      backgroundColor: "rgb(255, 99, 132)",
+      borderColor: "rgb(255, 99, 132)",
+      data: [0, 10, 5, 2, 20, 30, 45],
+    },
+  ],
+};
+
+var myChart = new Chart(document.getElementById("myChart"), config);
